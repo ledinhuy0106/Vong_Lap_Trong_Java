313c91a20156686a8199757d5d39923cb9b01c54
uyle1234 <91107969+uyle1234@users.noreply.github.com>
2021-10-08 16:53:09 +0700

    Add files via upload

--- Hienthicacsonguyentonhohon100.java
+++ Hienthicacsonguyentonhohon100.java
@@ -1,2 +1,18 @@
 public class Hienthicacsonguyentonhohon100 {
+    public static void main(String[] args) {
+        System.out.println("Các số nt nhỏ hơn 100 :" );
+        for (int i = 0;i<=100;i++ ){
+            if (check(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    public static boolean check(int n ){
+        if(n <2 )return false;
+        for (int i=2; i<=Math.sqrt(n);i++){
+            if(n%i==0)return false;
+        }
+        return true;
+    }
+
 }
